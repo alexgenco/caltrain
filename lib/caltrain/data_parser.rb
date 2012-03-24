52ecc9d4ccc58722497508c7c0406f607d70760b
Alex Genco <agenco@kabam.com>
2012-03-24 10:07:54 -0700

    fixed method chaining syntax

--- lib/caltrain/data_parser.rb
+++ lib/caltrain/data_parser.rb
@@ -5,11 +5,11 @@ module DataParser
     end
 
     def parse(file_path)
-      data[file_path] ||= File.read(file_path)
-      .split(/[\n\r]+/)[1..-1]
-      .map { |line| line.gsub('"', '')
-             .gsub(/\s+/, ' ')
-             .split(/,+/) }
+      data[file_path] ||= File.read(file_path).
+      split(/[\n\r]+/)[1..-1].
+      map { |line| line.gsub('"', '').
+             gsub(/\s+/, ' ').
+             split(/,+/) }
     end
   end
 end
